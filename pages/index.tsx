604355e9aacf28829be1a980422de71b6f2fbe8b
bue221 <camiloplaza3@gmail.com>
2022-04-27 00:34:06 -0500

    feat(add format style): add eslint and prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Button } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+export const semi = false;
+export const singleQuote = true;
+export const arrowParens = "avoid";
+export const trailingComma = "none";
+export const endOfLine = "auto";
